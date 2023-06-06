730687cb1dd53091b1d941c43854e8961a64e6dd
Zihao Li <zl81@st-andrews.ac.uk>
2023-06-07 00:39:34 +0800

    Add index creation script to allow user to create custom index

---
+++ data/create_index.py
@@ -0,0 +1,46 @@
+# Run this script and specify repos to create the index.bin file for the web app.
+from typing import List, Optional
+
+from docarray import BaseDoc, DocList
+from docarray.index import InMemoryExactNNIndex
+from docarray.typing import TorchTensor
+from transformers import pipeline
+
+
+REPOS = tuple(
+    input("Input repository names as owner/name, seperated by comma: ").split(",")
+)
+
+
+class RepoDoc(BaseDoc):
+    name: str
+    topics: List[str]
+    stars: int
+    license: str
+    code_embedding: Optional[TorchTensor[768]]
+    doc_embedding: Optional[TorchTensor[768]]
+
+
+model = pipeline(
+    model="Lazyhope/RepoSim",
+    trust_remote_code=True,
+    device_map="auto",
+)
+
+dl = DocList[RepoDoc]()
+repo_dataset = model(REPOS)
+
+for info in repo_dataset:
+    dl.append(
+        RepoDoc(
+            name=info["name"],
+            topics=info["topics"],
+            stars=info["stars"],
+            license=info["license"],
+            code_embedding=info["mean_code_embedding"],
+            doc_embedding=info["mean_doc_embedding"],
+        )
+    )
+
+index = InMemoryExactNNIndex[RepoDoc](dl)
+index.persist("index.bin")
